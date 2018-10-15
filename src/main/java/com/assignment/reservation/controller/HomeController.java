fc1764628b53267e098140f92dc38ac5fb74ef7a
Lahavas <uv187sn@naver.com>
2018-10-15 09:41:48 +0900

    fix: Fix bug that cannot call static resource

--- src/main/java/com/assignment/reservation/controller/HomeController.java
+++ src/main/java/com/assignment/reservation/controller/HomeController.java
@@ -6,12 +6,14 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
     @Autowired
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,6 @@ INSERT INTO room (id, name) VALUES (1, 'Room 1');
 INSERT INTO room (id, name) VALUES (2, 'Room 2');
 INSERT INTO room (id, name) VALUES (3, 'Room 3');
 INSERT INTO room (id, name) VALUES (4, 'Room 4');
+
+INSERT INTO reservation (id, room_id, owner, start, end) VALUES (1, 2, '주인', '2018-10-20T13:00:00', '2018-10-23T14:30:00');
+INSERT INTO reservation (id, room_id, owner, start, end) VALUES (2, 2, '다른 주인', '2018-10-18T13:00:00', '2018-10-20T12:30:00');
